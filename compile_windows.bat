83cb49befab1eb37d16e976d8eeafdee05f0c515
ajmd17 <andrewmaconald97@hotmail.com>
2016-07-14 14:28:49 -0700

    updated windows build script

--- compile_windows.bat
+++ compile_windows.bat
@@ -17,6 +17,9 @@ ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c sysroot/libc/std
 
 
 ghost-i686-elf-tools\bin\i686-elf-as.exe src/asm/arch/i386/crti.s -o obj/crti.o
+ghost-i686-elf-tools\bin\i686-elf-as.exe src/asm/gdt_flush.s -o obj/gdt_flush.o
+ghost-i686-elf-tools\bin\i686-elf-as.exe src/asm/idt_load.s -o obj/idt_load.o
+ghost-i686-elf-tools\bin\i686-elf-as.exe src/asm/isr_load.s -o obj/isr_load.o
 ghost-i686-elf-tools\bin\i686-elf-as.exe src/asm/boot.s -o obj/boot.o
 
 ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/terminal.c -o obj/terminal.o -ffreestanding -O2 -Wall -Wextra
@@ -25,11 +28,12 @@ ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/kerne
 ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/keyboard.c -o obj/keyboard.o -ffreestanding -O2 -Wall -Wextra
 ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/mouse.c -o obj/mouse.o -ffreestanding -O2 -Wall -Wextra
 ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/power.c -o obj/power.o -ffreestanding -O2 -Wall -Wextra
+ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/idt.c -o obj/idt.o -ffreestanding -O2 -Wall -Wextra
+ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/irq.c -o obj/irq.o -ffreestanding -O2 -Wall -Wextra
+ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/gdt.c -o obj/gdt.o -ffreestanding -O2 -Wall -Wextra
+ghost-i686-elf-tools\bin\i686-elf-g++.exe -Isysroot/include/ -c src/kernel/isrs.c -o obj/isrs.o -ffreestanding -O2 -Wall -Wextra
 
 
-ghost-i686-elf-tools\bin\i686-elf-g++.exe -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib obj/crti.o obj/boot.o obj/kernel.o obj/itoa.o obj/puts.o obj/putchar.o obj/printf.o obj/abort.o obj/memcmp.o obj/malloc.o obj/memcpy.o obj/memmove.o obj/memset.o obj/strlen.o obj/strcmp.o obj/terminal.o obj/vgascreen.o obj/keyboard.o obj/mouse.o obj/power.o -lgcc
-
-copy "myos.bin" "isodir\boot"
-copy "grub.cfg" "isodir\boot\grub"
+ghost-i686-elf-tools\bin\i686-elf-g++.exe -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib obj/crti.o obj/gdt_flush.o obj/isr_load.o obj/idt_load.o obj/boot.o obj/kernel.o obj/itoa.o obj/puts.o obj/putchar.o obj/printf.o obj/abort.o obj/memcmp.o obj/malloc.o obj/memcpy.o obj/memmove.o obj/memset.o obj/strlen.o obj/strcmp.o obj/terminal.o obj/vgascreen.o obj/keyboard.o obj/mouse.o obj/power.o obj/idt.o obj/irq.o obj/gdt.o obj/isrs.o -lgcc
 
 qemu-system-i386 -kernel myos.bin
